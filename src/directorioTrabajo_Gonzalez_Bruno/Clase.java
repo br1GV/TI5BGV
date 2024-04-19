22f0ba4eda3d572b516f821b21fdeccb828f25cd
unknown <DAM109@iesataulfo.local>
2024-04-19 16:28:30 +0200

    Cuarta fase proyecto (Clase1 creada, comentada y codificada y Principal creada y comentada)

--- src/directorioTrabajo_Gonzalez_Bruno/Clase.java
+++ src/directorioTrabajo_Gonzalez_Bruno/Clase.java
@@ -2,4 +2,7 @@ package directorioTrabajo_Gonzalez_Bruno;
 
 public class Clase {
 	// Constructor por defecto
+	public Clase() {
+        System.out.println("Soy un objeto de la clase y acabo de ser creado");
+    }
 }
