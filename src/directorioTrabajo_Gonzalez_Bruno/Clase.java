66e64519f399eb4cc3d99e9db24f9bcd3a891095
unknown <DAM109@iesataulfo.local>
2024-04-19 16:23:39 +0200

    Primera fase proyecto (Clase 1 y Principal creadas)

--- src/directorioTrabajo_Gonzalez_Bruno/Clase.java
+++ src/directorioTrabajo_Gonzalez_Bruno/Clase.java
@@ -1,7 +1,5 @@
 package directorioTrabajo_Gonzalez_Bruno;
 
 public class Clase {
-	public Clase() {
-        System.out.println("Soy un objeto de la clase y acabo de ser creado");
-    }
+	
 }
--- src/directorioTrabajo_Gonzalez_Bruno/Principal.java
+++ src/directorioTrabajo_Gonzalez_Bruno/Principal.java
@@ -3,8 +3,6 @@ package directorioTrabajo_Gonzalez_Bruno;
 public class Principal {
 
 	public static void main(String[] args) {
-		// Crear un objeto de la clase Clase
-        Clase objetoClase = new Clase();
 
 	}
 
